cb753d2a42a38b1c8e47871cd9b9aca270e226f9
Carlos Eduardo Rosa Machado <carlos.rmachado@ymail.com>
2011-09-05 17:07:08 -0300

    Adding server statistics.

--- lab1/server_echo.c
+++ lab1/server_echo.c
@@ -23,6 +23,7 @@ main()
     struct sockaddr_in my_addr;    /* my address information */
     struct sockaddr_in their_addr; /* connector's address information */
     char recvline[MAXDATASIZE];
+    int num_rcvd, lines_rcvd;
     int sin_size, numbytes, yes;
 
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
@@ -66,7 +67,10 @@ main()
         /* Maintain connection in a new thread: */
         if (!fork()) {
             /* Main connection loop - echo received data: */
+            num_rcvd = lines_rcvd = 0;
             while ((numbytes = recv(new_fd, recvline, MAXDATASIZE, 0)) > 0) {
+                lines_rcvd++;
+                num_rcvd += numbytes;
                 if (send(new_fd, recvline, numbytes, 0) == -1) {
                     perror("send");
                     exit(1);
@@ -79,6 +83,10 @@ main()
             }
 
             close(new_fd);
+
+            /* Print statistics: */
+            fprintf(stderr, "Number of lines received: %d\n", lines_rcvd);
+            fprintf(stderr, "Number of characters received: %d\n", num_rcvd);
             exit(0);
         }
         close(new_fd);  /* parent doesn't need this */

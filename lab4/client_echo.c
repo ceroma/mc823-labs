2d06ad133ee33a21536c60ad0b20129d772920e8
Carlos Eduardo Rosa Machado <carlos.rmachado@ymail.com>
2011-10-30 15:03:31 -0700

    Initializing lab4 folder.

---
+++ lab4/client_echo.c
@@ -0,0 +1,155 @@
+/*
+** client.c -- a stream socket client demo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/times.h>
+#include <sys/wait.h>
+
+#define PORT        3490 /* the port client will be connecting to */
+#define MAXDATASIZE 256  /* max number of bytes we can get at once */
+
+int main(int argc, char *argv[])
+{
+    clock_t t1, t2;
+    double time_diff;
+    int sockfd;
+    int num_sent, num_rcvd;
+    int size_sent, max_size;
+    int lines_sent, lines_rcvd;
+    char buf[MAXDATASIZE];
+    struct hostent *he;
+    struct sockaddr_in their_addr; /* connector's address information */
+    FILE *rsock, *wsock;
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: client hostname\n");
+        exit(1);
+    }
+
+    if ((he = gethostbyname(argv[1])) == NULL) {  /* get the host info */
+        perror("gethostbyname");
+        exit(1);
+    }
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    their_addr.sin_family = AF_INET;         /* host byte order */
+    their_addr.sin_port = htons(PORT);       /* short, network byte order */
+    their_addr.sin_addr = *((struct in_addr *)he->h_addr);
+    bzero(&(their_addr.sin_zero), 8);        /* zero the rest of the struct */
+
+    if (connect(sockfd, (struct sockaddr *)&their_addr, sizeof(struct sockaddr))
+        == -1) {
+        perror("connect");
+        exit(1);
+    }
+
+    /* Start counter: */
+    if ((t1 = times(NULL)) == (clock_t) -1) {
+        perror("times");
+        exit(1);
+    }
+
+    /* Child process sends stream, parent reads: */
+    if (!fork()) {
+        /* Associate a write stream with the socket file descriptor: */
+        if ((wsock = fdopen(sockfd, "w")) == NULL) {
+            perror("fdopen");
+            exit(1);
+        }
+
+        /* Read stdin until EOF: */
+        num_sent = lines_sent = max_size = 0;
+        while (fgets(buf, MAXDATASIZE, stdin)) {
+            /* Send data to server: */
+            if (fputs(buf, wsock) == EOF) {
+                fprintf(stderr, "fputs: error writing to socket\n");
+                exit(1);
+            }
+            if (fflush(wsock) == EOF) {
+                perror("fflush");
+                exit(1);
+            }
+
+            /* Statistics - send: */
+            lines_sent++;
+            size_sent = strlen(buf);
+            num_sent += size_sent;
+            max_size = (size_sent > max_size) ? size_sent : max_size;
+        }
+
+        /* Disallow further transmissions: */
+        if (shutdown(sockfd, SHUT_WR) == -1) {
+            perror("shutdown");
+            exit(1);
+        }
+        free(wsock);
+
+        /* Print statistics: */
+        fprintf(stderr, "Number of lines sent: %d\n", lines_sent);
+        fprintf(stderr, "Size of longest line: %d\n", max_size);
+        fprintf(stderr, "Number of characters sent: %d\n", num_sent);
+
+        return 0;
+    }
+
+    /* Associate a read stream with the socket file descriptor: */
+    if ((rsock = fdopen(sockfd, "r")) == NULL) {
+        perror("fdopen");
+        exit(1);
+    }
+
+    /* Echo data received from server: */
+    num_rcvd = lines_rcvd = 0;
+    while(fgets(buf, MAXDATASIZE, rsock)) {
+        if (fflush(rsock) == EOF) {
+            perror("fflush");
+            exit(1);
+        }
+        fputs(buf, stdout);
+
+        /* Statistics - recv: */
+        lines_rcvd++;
+        num_rcvd += strlen(buf);
+    }
+
+    /* Print statistics: */
+    fprintf(stderr, "Number of lines received: %d\n", lines_rcvd);
+    fprintf(stderr, "Number of characters received: %d\n", num_rcvd);
+
+    /* Wait for child process: */
+    if (wait(NULL) == -1) {
+        perror("wait");
+        exit(1);
+    }
+
+    if (close(sockfd) == -1) {
+        perror("close");
+        exit(1);
+    }
+    free(rsock);
+
+    /* Stop counter: */
+    if ((t2 = times(NULL)) == (clock_t) -1) {
+        perror("times");
+        exit(1);
+    }
+
+    /* Print statistics: */
+    time_diff = ((double) t2 - (double) t1) / sysconf(_SC_CLK_TCK);
+    fprintf(stderr, "Time: %.1lfs\n", time_diff);
+
+    return 0;
+}
---
+++ lab4/server_echo.c
@@ -0,0 +1,120 @@
+/*
+** server.c -- a stream socket server demo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+
+#define MYPORT      3490 /* the port users will be connecting to */
+#define BACKLOG     10   /* how many pending connections queue will hold */
+#define MAXDATASIZE 256  /* max number of bytes we can get at once */
+
+main()
+{
+    int sockfd, new_fd;  /* listen on sock_fd, new connection on new_fd */
+    struct sockaddr_in my_addr;    /* my address information */
+    struct sockaddr_in their_addr; /* connector's address information */
+    char recvline[MAXDATASIZE];
+    int num_rcvd, lines_rcvd;
+    int sin_size, yes;
+    FILE *rsock, *wsock;
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+        perror("setsockopt");
+        exit(1);
+    }
+
+    my_addr.sin_family = AF_INET;         /* host byte order */
+    my_addr.sin_port = htons(MYPORT);     /* short, network byte order */
+    my_addr.sin_addr.s_addr = INADDR_ANY; /* automatically fill with my IP */
+    bzero(&(my_addr.sin_zero), 8);        /* zero the rest of the struct */
+
+    if (-1 ==
+        bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr))) {
+        perror("bind");
+        exit(1);
+    }
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    while (1) {  /* main accept() loop */
+        sin_size = sizeof(struct sockaddr_in);
+        if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size))
+            == -1) {
+            perror("accept");
+            continue;
+        }
+
+        printf(
+            "server: got connection from %s\n",
+            inet_ntoa(their_addr.sin_addr)
+        );
+
+        /* Associate a write stream with the socket file descriptor: */
+        if ((wsock = fdopen(new_fd, "w")) == NULL) {
+            perror("fdopen");
+            exit(1);
+        }
+
+        /* Associate a read stream with the socket file descriptor: */
+        if ((rsock = fdopen(new_fd, "r")) == NULL) {
+            perror("fdopen");
+            exit(1);
+        }
+
+        /* Maintain connection in a new thread: */
+        if (!fork()) {
+            /* Main connection loop - echo received data: */
+            num_rcvd = lines_rcvd = 0;
+            while (fgets(recvline, MAXDATASIZE, rsock)) {
+                if (fflush(rsock) == EOF) {
+                    perror("fflush");
+                    exit(1);
+                }
+                lines_rcvd++;
+                num_rcvd += strlen(recvline);
+                if (fputs(recvline, wsock) == EOF) {
+                    fprintf(stderr, "fputs: error writing to socket\n");
+                    exit(1);
+                }
+                if (fflush(wsock) == EOF) {
+                    perror("fflush");
+                    exit(1);
+                }
+            }
+            if (close(new_fd) == -1) {
+                perror("close");
+                exit(1);
+            }
+            free(rsock);
+            free(wsock);
+
+            /* Print statistics: */
+            fprintf(stderr, "Number of lines received: %d\n", lines_rcvd);
+            fprintf(stderr, "Number of characters received: %d\n", num_rcvd);
+            exit(0);
+        }
+        if (close(new_fd) == -1) {  /* parent doesn't need this */
+            perror("close");
+            exit(1);
+        }
+
+        /* Clean up all child processes: */
+        while (waitpid(-1, NULL, WNOHANG) > 0);
+    }
+}

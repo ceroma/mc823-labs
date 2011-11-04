752623cdfb37544e9613d98b65efb3e97db95e28
Carlos Eduardo Rosa Machado <carlos.rmachado@ymail.com>
2011-11-04 15:20:43 -0700

    Daemonizing server.

--- lab4/server_echo.c
+++ lab4/server_echo.c
@@ -2,12 +2,16 @@
 ** server.c -- a stream socket server demo
 */
 
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <signal.h>
 #include <string.h>
+#include <syslog.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
@@ -16,6 +20,112 @@
 #define MYPORT      3490 /* the port users will be connecting to */
 #define BACKLOG     10   /* how many pending connections queue will hold */
 #define MAXDATASIZE 256  /* max number of bytes we can get at once */
+
+/**
+ * Daemonizes the server.
+ */
+void daemon_init() {
+    int i = 0;
+    pid_t pid;
+
+    /* Terminate parent: */
+    if ((pid = fork()) == -1) {
+        perror("fork");
+        exit(1);
+    } else if (pid != 0) {
+        exit(0);
+    }
+
+    /* Become session leader, detaching from terminal: */
+    if (setsid() == -1) {
+        perror("setsid");
+        exit(1);
+    }
+
+    /* Ignore children termination: */
+    if (signal(SIGHUP, SIG_IGN) == SIG_ERR) {
+        perror("signal");
+        exit(1);
+    }
+
+    /* Terminate first child: */
+    if ((pid = fork()) == -1) {
+        perror("fork");
+        exit(1);
+    } else if (pid != 0) {
+        exit(0);
+    }
+
+    /* Change working directory: */
+    if (chdir("/tmp") == -1) {
+        perror("chdir");
+        exit(1);
+    }
+
+    /* Clear file mode creation mask: */
+    umask(0);
+
+    /* Close all file descriptors: */
+    do { close(i); } while (++i < getdtablesize());
+
+    /* Tell syslog to include PID with each message: */
+    openlog("server-echo", LOG_PID, 0);
+}
+
+/**
+ * Logs message to a private file.
+ */
+void log_message(char *message) {
+    FILE *fp;
+    time_t now;
+    char buf[64];
+
+    /* Build log header: */
+    time(&now);
+    sprintf(buf, "%sserver-echo[%d]: ", ctime(&now), getpid());
+
+    /* Open private log file: */
+    if ((fp = fopen("SERVER.LOG", "a")) == 0) {
+        exit(1);
+    }
+
+   /* Logs message: */
+   fprintf(fp, "%s %s\n", buf, message);
+   fclose(fp);
+}
+
+/**
+ * Logs a new connection to the server.
+ */
+void log_conn(struct in_addr in) {
+    //syslog(LOG_USER | LOG_INFO, "Got connection from: %s\n", inet_ntoa(in));
+    char buf[64];
+    sprintf(buf, "Got connection from: %s\n", inet_ntoa(in));
+    log_message(buf);
+}
+
+/**
+ * Logs connection's statistics.
+ */
+void log_stats(int num_rcvd, int lin_rcvd) {
+    //syslog(LOG_USER | LOG_INFO, "Number of bytes received: %d\n", num_rcvd);
+    //syslog(LOG_USER | LOG_INFO, "Number of lines received: %d\n", lin_rcvd);
+    char buf[64];
+    sprintf(buf, "Number of bytes received: %d\n", num_rcvd);
+    log_message(buf);
+    sprintf(buf, "Number of lines received: %d\n", lin_rcvd);
+    log_message(buf);
+}
+
+/**
+ * Logs an execution error.
+ */
+void log_error(char *fname) {
+    //syslog(LOG_USER | LOG_ERR, "%s: %m\n", fname);
+    char buf[64];
+    sprintf(buf, "%s: %s\n", fname, strerror(errno));
+    log_message(buf);
+}
 
 main()
 {
@@ -27,12 +137,14 @@ main()
     int sin_size, yes;
     FILE *rsock, *wsock;
 
+    daemon_init();
+
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("socket");
+        log_error("socket");
         exit(1);
     }
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-        perror("setsockopt");
+        log_error("setsockopt");
         exit(1);
     }
 
@@ -43,12 +155,12 @@ main()
 
     if (-1 ==
         bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr))) {
-        perror("bind");
+        log_error("bind");
         exit(1);
     }
 
     if (listen(sockfd, BACKLOG) == -1) {
-        perror("listen");
+        log_error("listen");
         exit(1);
     }
 
@@ -56,24 +168,21 @@ main()
         sin_size = sizeof(struct sockaddr_in);
         if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size))
             == -1) {
-            perror("accept");
+            log_error("accept");
             continue;
         }
 
-        printf(
-            "server: got connection from %s\n",
-            inet_ntoa(their_addr.sin_addr)
-        );
+        log_conn(their_addr.sin_addr);
 
         /* Associate a write stream with the socket file descriptor: */
         if ((wsock = fdopen(new_fd, "w")) == NULL) {
-            perror("fdopen");
+            log_error("fdopen");
             exit(1);
         }
 
         /* Associate a read stream with the socket file descriptor: */
         if ((rsock = fdopen(new_fd, "r")) == NULL) {
-            perror("fdopen");
+            log_error("fdopen");
             exit(1);
         }
 
@@ -83,34 +192,33 @@ main()
             num_rcvd = lines_rcvd = 0;
             while (fgets(recvline, MAXDATASIZE, rsock)) {
                 if (fflush(rsock) == EOF) {
-                    perror("fflush");
+                    log_error("fflush");
                     exit(1);
                 }
                 lines_rcvd++;
                 num_rcvd += strlen(recvline);
                 if (fputs(recvline, wsock) == EOF) {
-                    fprintf(stderr, "fputs: error writing to socket\n");
+                    log_error("fputs: error writing to socket");
                     exit(1);
                 }
                 if (fflush(wsock) == EOF) {
-                    perror("fflush");
+                    log_error("fflush");
                     exit(1);
                 }
             }
             if (close(new_fd) == -1) {
-                perror("close");
+                log_error("close");
                 exit(1);
             }
             free(rsock);
             free(wsock);
 
             /* Print statistics: */
-            fprintf(stderr, "Number of lines received: %d\n", lines_rcvd);
-            fprintf(stderr, "Number of characters received: %d\n", num_rcvd);
+            log_stats(num_rcvd, lines_rcvd);
             exit(0);
         }
         if (close(new_fd) == -1) {  /* parent doesn't need this */
-            perror("close");
+            log_error("close");
             exit(1);
         }
 

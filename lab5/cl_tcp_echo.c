81f0436e650994f9f7ca24f15073559608a07588
Carlos Eduardo Rosa Machado <ra059582@xaveco.lab.ic.unicamp.br>
2011-11-17 05:25:26 -0200

    Using new functions on echo server/client.

--- lab5/cl_tcp_echo.c
+++ lab5/cl_tcp_echo.c
@@ -4,15 +4,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/times.h>
-#include <sys/wait.h>
+#include <unistd.h>
+#include "cl_utils.h"
 
 #define PORT        7331 /* the port client will be connecting to */
 #define MAXDATASIZE 256  /* max number of bytes we can get at once */
@@ -27,8 +23,6 @@ int main(int argc, char *argv[])
     int size_sent, max_size;
     int lines_sent, lines_rcvd;
     char buf[MAXDATASIZE];
-    struct hostent *he;
-    struct sockaddr_in their_addr; /* connector's address information */
     FILE *rsock, *wsock;
 
     if (argc != 2) {
@@ -46,26 +40,8 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    if ((he = gethostbyname(argv[1])) == NULL) {  /* get the host info */
-        perror("gethostbyname");
-        exit(1);
-    }
-
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("socket");
-        exit(1);
-    }
-
-    their_addr.sin_family = AF_INET;         /* host byte order */
-    their_addr.sin_port = htons(PORT);       /* short, network byte order */
-    their_addr.sin_addr = *((struct in_addr *)he->h_addr);
-    bzero(&(their_addr.sin_zero), 8);        /* zero the rest of the struct */
-
-    if (connect(sockfd, (struct sockaddr *)&their_addr, sizeof(struct sockaddr))
-        == -1) {
-        perror("connect");
-        exit(1);
-    }
+    /* Connect to server: */
+    sockfd = cl_tcp_socket(argv[1], PORT);
 
     /* Associate a write stream with the socket file descriptor: */
     if ((wsock = fdopen(sockfd, "w")) == NULL) {
--- lab5/sv_tcp_echo.c
+++ lab5/sv_tcp_echo.c
@@ -4,65 +4,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/wait.h>
-#include <arpa/inet.h>
+#include "sv_utils.h"
 
 #define MYPORT      7331 /* the port users will be connecting to */
-#define BACKLOG     10   /* how many pending connections queue will hold */
 #define MAXDATASIZE 256  /* max number of bytes we can get at once */
 
 int main(int argc, char *argv[])
 {
     int sockfd, new_fd;  /* listen on sock_fd, new connection on new_fd */
-    struct sockaddr_in my_addr;    /* my address information */
-    struct sockaddr_in their_addr; /* connector's address information */
     char recvline[MAXDATASIZE];
     int num_rcvd, lines_rcvd;
-    int sin_size, yes;
     FILE *rsock, *wsock;
 
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("socket");
-        exit(1);
-    }
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-        perror("setsockopt");
-        exit(1);
-    }
-
-    my_addr.sin_family = AF_INET;         /* host byte order */
-    my_addr.sin_port = htons(MYPORT);     /* short, network byte order */
-    my_addr.sin_addr.s_addr = INADDR_ANY; /* automatically fill with my IP */
-    bzero(&(my_addr.sin_zero), 8);        /* zero the rest of the struct */
-
-    if (-1 ==
-        bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr))) {
-        perror("bind");
-        exit(1);
-    }
-
-    if (listen(sockfd, BACKLOG) == -1) {
-        perror("listen");
-        exit(1);
-    }
-
-    sin_size = sizeof(struct sockaddr_in);
-    if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size))
-        == -1) {
-        perror("accept");
-        exit(1);
-    }
-
-    printf(
-        "server: got connection from %s\n",
-        inet_ntoa(their_addr.sin_addr)
-    );
+    /* Accept new connection: */
+    sockfd = tcp_socket(MYPORT);
+    new_fd = tcp_accept(sockfd);
 
     /* Associate a write stream with the socket file descriptor: */
     if ((wsock = fdopen(new_fd, "w")) == NULL) {

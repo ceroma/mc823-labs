6474407270deaeca19b9eadd77496118dd674599
Carlos Eduardo Rosa Machado <ra059582@xaveco.lab.ic.unicamp.br>
2011-12-06 06:22:19 -0200

    Reading and printing myinetd's services.

---
+++ lab5/myinetd.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "myinetd.h"
+
+/**
+ * Reads the current line of myinetd.conf and returns a related service_t.
+ */
+service_t read_service(char * line) {
+    service_t service;
+    char * field;
+
+    /* Read name: */
+    field = strtok(line, SPACE_DELIMS);
+    strcpy(service.name, field); 
+
+    /* Read port: */
+    field = strtok(NULL, SPACE_DELIMS);
+    service.port = atoi(field);
+
+    /* Read protocol: */
+    strtok(NULL, SPACE_DELIMS);
+    field = strtok(NULL, SPACE_DELIMS);
+    if (!strcmp(field, "tcp")) {
+        service.protocol = TCP;
+    } else {
+        service.protocol = UDP;
+    }
+
+    /* Read pathname: */
+    strtok(NULL, SPACE_DELIMS);
+    field = strtok(NULL, SPACE_DELIMS);
+    strcpy(service.path, field); 
+
+    return service;
+}
+
+/**
+ * Reads and stores all services in myinetd.conf.
+ */
+services_t read_config() {
+    FILE * conf;
+    services_t s;
+    char line[MAX_LINE_SIZE];
+
+    /* Open config file: */
+    conf = fopen(MYINETD_CONF_PATH, "r");
+
+    /* Read services in config file: */
+    s.N = 0;
+    s.service = NULL;
+    while (fgets(line, MAX_LINE_SIZE, conf)) {
+        s.N++;
+        s.service = realloc(s.service, s.N * sizeof(service_t));
+        s.service[s.N - 1] = read_service(line);
+    }
+    
+    fclose(conf);
+    return s;
+}
+
+int main(int argc, char *argv[]) {
+    int i;
+    services_t s;
+
+    /* Reads myinetd.conf: */
+    s = read_config();
+
+    /* Verify read data: */
+    printf("Services:\n");
+    printf("i: Name\t\tPort\tType\tPath\n");
+    for (i = 0; i < s.N; i++) {
+        printf(
+            "%d: %s\t%d\t%s\t%s\n",
+            i,
+            s.service[i].name,
+            s.service[i].port,
+            s.service[i].protocol == TCP ? "tcp" : "udp",
+            s.service[i].path
+        );
+    }
+
+    /* Free everything: */
+    free(s.service);
+
+    return 0;
+}
---
+++ lab5/myinetd.h
@@ -0,0 +1,37 @@
+#include "sv_utils.h"
+
+#define MAX_NAME_SIZE 20
+#define MAX_PATH_SIZE 100
+#define MAX_LINE_SIZE 200
+
+#define SPACE_DELIMS " \t\n"
+
+#define MYINETD_CONF_PATH "./myinetd.conf"
+
+/**
+ * Myinetd service type.
+ */
+typedef struct {
+    int port;
+    socktype_t protocol;
+    char name[MAX_NAME_SIZE];
+    char path[MAX_PATH_SIZE];
+} service_t;
+
+/**
+ * List of services.
+ */
+typedef struct {
+    int N;
+    service_t * service;
+} services_t;
+
+/**
+ * Reads the current line of myinetd.conf and returns a related service_t.
+ */
+service_t read_service(char * line);
+
+/**
+ * Reads and stores all services in myinetd.conf.
+ */
+services_t read_config();
